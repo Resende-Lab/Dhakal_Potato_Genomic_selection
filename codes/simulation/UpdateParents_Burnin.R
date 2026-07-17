b45ff4b77b297e8eec277cb48f58c9875fe8ee10
rakshyadkl <73804389+rakshyadkl@users.noreply.github.com>
2026-07-17 13:14:51 -0400

    Update UpdateParents.R

--- codes/simulation/UpdateParents_Burnin.R
+++ codes/simulation/UpdateParents_Burnin.R
@@ -1,8 +1,4 @@
-##############################################################################
-# UpdateParents_Burnin.R — Select new parents during burn-in
-#
-# Selects top 50 from Third Clonal stage based on selection index.
-##############################################################################
+# Update of the parents at each cycle
 
-Parents <- selectInd(Second_Clonal, 50,
-                     trait = selIndex, b = weight, scale = TRUE)
+Parents = selectInd(Second_Clonal, 50,
+                    trait = selIndex, b = weight, scale = TRUE) ####
