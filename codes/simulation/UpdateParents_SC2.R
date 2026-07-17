81f9de2ca51610b8961624354118649097ebc9b4
rakshyadkl <73804389+rakshyadkl@users.noreply.github.com>
2026-07-17 13:17:40 -0400

    Create UpdateParents_SC2.R

---
+++ codes/simulation/UpdateParents_SC2.R
@@ -0,0 +1,7 @@
+# Update of the parents at each cycle
+Parents = c(selectInd(Second_Clonal, 28,
+                      trait = selIndex, b = weight, scale = TRUE),
+            selectInd(Third_Clonal, 15,
+                      trait = selIndex, b = weight, scale = TRUE),
+            selectInd(Fouth_Clonal, 7,
+                      trait = selIndex, b = weight, scale = TRUE))
